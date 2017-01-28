35eb301e6c9b7b12b93568d12c91763cfc540238
Robert Habermann <mail@rhab.de>
2017-01-28 21:53:12 +0000

    switch to Python3 - too much unicode stuff to handle

--- faq_search_cli.py
+++ faq_search_cli.py
@@ -21,9 +21,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-from __future__ import print_function
-from __future__ import unicode_literals
-
 from elasticsearch import Elasticsearch, RequestsHttpConnection, serializer, compat, exceptions
 import config
 import json
